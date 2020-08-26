f1eecab7a22930f448c126fc94ffd8a3fd50f26a
luoxiu <luoxiustm@gmail.com>
2020-08-26 09:45:59 +0800

    add DummyLogHandler

---
+++ Sources/LogDog/DummyLogHandler.swift
@@ -0,0 +1,14 @@
+import Logging
+
+public struct DummyLogHandler: LogHandler {
+    public var metadata: Logger.Metadata
+    public var logLevel: Logger.Level
+    
+    public subscript(metadataKey metadataKey: String) -> Logger.Metadata.Value? {
+        get { metadata[metadataKey] }
+        set { metadata[metadataKey] = newValue }
+    }
+    
+    public func log(level: Logger.Level, message: Logger.Message, metadata: Logger.Metadata?, source: String, file: String, function: String, line: UInt) {
+    }
+}
