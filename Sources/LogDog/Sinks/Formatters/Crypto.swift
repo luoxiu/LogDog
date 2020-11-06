ffb616fb13a362ce9b1ff6da07a5a3aabc68a310
luoxiu <luoxiustm@gmail.com>
2020-11-07 00:18:38 +0800

    Crypto should only be on macos

--- Sources/LogDog/Sinks/Formatters/Crypto.swift
+++ Sources/LogDog/Sinks/Formatters/Crypto.swift
@@ -1,28 +1,31 @@
-import CryptoKit
 import Foundation
 
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
-public extension LogSink where Self.Output == Data {
-    func encrypt(using key: SymmetricKey) -> LogSinks.Concat<Self, LogFormatters.Crypto> {
-        self + LogFormatters.Crypto(key: key)
-    }
-}
+#if canImport(CryptoKit)
+    import CryptoKit
 
-public extension LogFormatters {
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
-    struct Crypto: LogFormatter {
-        public typealias Input = Data
-        public typealias Output = Data
-
-        public let key: SymmetricKey
-
-        public init(key: SymmetricKey) {
-            self.key = key
-        }
-
-        public func format(_ record: LogRecord<Data>) throws -> Data? {
-            let box = try ChaChaPoly.seal(record.output, using: key)
-            return box.combined
+    public extension LogSink where Self.Output == Data {
+        func encrypt(using key: SymmetricKey) -> LogSinks.Concat<Self, LogFormatters.Crypto> {
+            self + LogFormatters.Crypto(key: key)
         }
     }
-}
+
+    public extension LogFormatters {
+        @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+        struct Crypto: LogFormatter {
+            public typealias Input = Data
+            public typealias Output = Data
+
+            public let key: SymmetricKey
+
+            public init(key: SymmetricKey) {
+                self.key = key
+            }
+
+            public func format(_ record: LogRecord<Data>) throws -> Data? {
+                let box = try ChaChaPoly.seal(record.output, using: key)
+                return box.combined
+            }
+        }
+    }
+#endif
